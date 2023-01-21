7cb0a56b0413d81c945e9a52a1e5dcaf99f29ff7
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-20 18:22:36 -0600

    beanie baby array shows

--- app.js
+++ app.js
@@ -8,15 +8,23 @@ const beanieBabiesEl = document.querySelector('.beanie-babies');
 let beanieBabiesData = [];
 let astrologyData = []; 
 
-const SUPABASE_URL = "https://gxwgjhfyrlwiqakdeamc.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA";
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Events */
 window.addEventListener('load', async () => {
     let { data, error } = await client
-        .from('beanie_babies').select('*');
+        .from('beanie_babies').select('*').limit(100);
+
+    for (let beanieBaby of data) {
+        const beanieBabyEl = document.createElement('div');
+        
+
+        beanieBabyEl.textContent = `${beanieBaby.title}sign is${beanieBaby.astroSign}`;
+        beanieBabiesEl.append(beanieBabyEl);
+    }
 });
 /* Display Functions */
 
